5c0272cb1179c0561278ced1b60bab286ab0cb3d
Philip Wilk <wiryfuture@gmail.com>
2022-11-14 20:34:57 +0000

    fix armhf build by adding amd64 dependency ofc?

--- cross/armv7hf.sh
+++ cross/armv7hf.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 mv /etc/apt/sources.list /etc/apt/sources.list.bak
-mv /etc/apt/sources.d /etc/apt/sources.d.bak
-echo -e "deb http://ftp.debian.org/debian sid main" > /etc/apt/sources.list
+echo -e "deb http://ftp.debian.org/debian sid main\ndeb http://ftp.debian.org/debian sid main" > /etc/apt/sources.list
 dpkg --add-architecture armhf
 dpkg --add-architecture amd64
 apt-get update
@@ -10,6 +9,6 @@ apt-get -d --no-install-recommends download libssl-dev:armhf
 dpkg -x libssl-dev* /
 rm libssl-dev*
 apt-get -d --no-install-recommends download libssl-dev:amd64
+dpkg -x libssl-dev* /
 mv -f /etc/apt/sources.list.bak /etc/apt/sources.list
-mv -f /etc/apt/sources.d.bak /etc/apt/sources.d
 dpkg --remove-architecture armhf
