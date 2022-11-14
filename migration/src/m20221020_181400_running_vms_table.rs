4dc54397663d18464dbbbb0b8a27503f498bca51
Philip Wilk <wiryfuture@gmail.com>
2022-11-14 18:32:22 +0000

    linting + make release smol

--- migration/src/m20221020_181400_running_vms_table.rs
+++ migration/src/m20221020_181400_running_vms_table.rs
@@ -1,6 +1,6 @@
 use sea_orm_migration::prelude::*;
 
-use crate::identities::{RunningVms, Uuid, Hosts, Vms};
+use crate::identities::{Hosts, RunningVms, Uuid, Vms};
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -20,9 +20,17 @@ impl MigrationTrait for Migration {
                             .primary_key(),
                     )
                     .col(ColumnDef::new(RunningVms::State).boolean().not_null())
-                    .col(ColumnDef::new(RunningVms::Host).custom(Uuid::Uuid).not_null())
+                    .col(
+                        ColumnDef::new(RunningVms::Host)
+                            .custom(Uuid::Uuid)
+                            .not_null(),
+                    )
                     .col(ColumnDef::new(RunningVms::IsOrphan).boolean().not_null())
-                    .col(ColumnDef::new(RunningVms::VmUuid).custom(Uuid::Uuid).not_null())
+                    .col(
+                        ColumnDef::new(RunningVms::VmUuid)
+                            .custom(Uuid::Uuid)
+                            .not_null(),
+                    )
                     .col(ColumnDef::new(RunningVms::Fd).text().not_null())
                     .foreign_key(
                         ForeignKey::create()
@@ -30,7 +38,7 @@ impl MigrationTrait for Migration {
                             .from(RunningVms::Table, RunningVms::Host)
                             .to(Hosts::Table, Hosts::Uuid)
                             .on_delete(ForeignKeyAction::SetNull)
-                            .on_update(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
                     )
                     .foreign_key(
                         ForeignKey::create()
@@ -38,7 +46,7 @@ impl MigrationTrait for Migration {
                             .from(RunningVms::Table, RunningVms::VmUuid)
                             .to(Vms::Table, Vms::Uuid)
                             .on_delete(ForeignKeyAction::SetNull)
-                            .on_update(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
                     )
                     .to_owned(),
             )
