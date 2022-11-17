7d3a60b2bd0535615ae7943c9613164195b34e17
Philip Wilk <wiryfuture@gmail.com>
2022-11-17 12:03:23 +0000

    use ptr::null()

--- src/kvm.rs
+++ src/kvm.rs
@@ -1,20 +1,35 @@
-use nix::{ioctl_write_int_bad, request_code_none, sys::mman::mmap, libc::{MAP_ANONYMOUS, MAP_NORESERVE, PROT_READ, PROT_WRITE, MAP_PRIVATE}};
+use nix::{
+    ioctl_write_int_bad,
+    libc::{MAP_ANONYMOUS, MAP_NORESERVE, MAP_PRIVATE, PROT_READ, PROT_WRITE},
+    request_code_none, request_code_write,
+    sys::mman::{mmap, MapFlags, ProtFlags},
+};
 use std::{
     fs::OpenOptions,
     os::unix::prelude::{IntoRawFd, RawFd},
+    ptr,
 };
 
-use crate::kvm_consts::{KVM_CREATE_VM, KVM_IOCTL_ID, KvmUserspaceMemoryRegion};
+use crate::kvm_consts::{
+    KvmUserspaceMemoryRegion, KVM_CREATE_VM, KVM_IOCTL_ID, KVM_SET_USER_MEMORY_REGION,
+};
 
 /*
     create vm file descriptor, may have addition parameter for machine type, but is only relevant on S390 and arm64 platforms
 */
-pub async fn create_vm(kvm_fd: RawFd) {
+pub async fn create_vm(kvm_fd: RawFd, memory_size: usize) {
     ioctl_write_int_bad!(
         create_vm_fd,
         request_code_none!(KVM_IOCTL_ID, KVM_CREATE_VM)
     );
+    // create the vm
     unsafe { create_vm_fd(kvm_fd, 0).expect("create_vm_fd_ERROR") };
+    // assign memory to vm
+    let ram_region: KvmUserspaceMemoryRegion = define_ram_region(8192, memory_size).await.unwrap();
+    ioctl_write_int_bad!(
+        set_memory_region,
+        request_code_write!(KVM_IOCTL_ID, KVM_SET_USER_MEMORY_REGION, &|| { ram_region })
+    );
 }
 
 /*
@@ -22,9 +37,9 @@ pub async fn create_vm(kvm_fd: RawFd) {
     Result -> Done, invalid data input or error
 */
 pub async fn define_ram_region(
-    available_capacity: u64,
-    guest_capacity: u64,
-) -> Result<bool, String> {
+    available_capacity: usize,
+    guest_capacity: usize,
+) -> Result<KvmUserspaceMemoryRegion, String> {
     // check that available mem is not lower than guest memory, and positive
     if guest_capacity < available_capacity {
         return Err("Requested more memory for guest than is available on host".to_string());
@@ -33,21 +48,22 @@ pub async fn define_ram_region(
             "Requested less memory than minimum of 1Mb: ".to_string() + &guest_capacity.to_string()
         );
     }
-    // convert mebibytes to bytes 
-    let memory_size: u64 = guest_capacity * 1048576;
+    // convert mebibytes to bytes
+    let memory_size: usize = guest_capacity * 1048576;
     /*
-        TODO: check out mmap implementation so the null value is correct
+        TODO: Check protflagss and mapflags implementations to correctly perform bitwise
     */
-    let mem = unsafe { mmap(NULL, memory_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE, -1, 0)};
-    let kvm_userspace_memory_region = KvmUserspaceMemoryRegion{
+    let prot_flags: ProtFlags = PROT_READ | PROT_WRITE;
+    let map_flags: MapFlags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE;
+    let mem = unsafe { mmap(*ptr::null(), memory_size, prot_flags, map_flags, -1, 0) };
+
+    Ok(KvmUserspaceMemoryRegion {
         slot: 0,
         flags: 0,
         guest_phys_addr: 0,
         memory_size: memory_size,
         userspace_addr: mem,
-    }
-    
-    Ok(true)
+    })
 }
 
 pub async fn get_kvm_fd() -> RawFd {
