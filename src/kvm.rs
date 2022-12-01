e5515aeea94b648b7b2ea1ff780f9e03da4a0107
Philip Wilk <wiryfuture@gmail.com>
2022-12-01 18:52:26 +0000

    update nix ver, sea orm ver and move mem struct out of define fn

--- src/kvm.rs
+++ src/kvm.rs
@@ -7,8 +7,11 @@ use nix::{
 use std::{
     fs::OpenOptions,
     mem::size_of,
-    os::unix::prelude::{IntoRawFd, RawFd},
-    ptr::null_mut,
+    num::NonZeroUsize,
+    os::{
+        raw::c_void,
+        unix::prelude::{IntoRawFd, RawFd},
+    },
 };
 
 use crate::kvm_consts::{
@@ -27,6 +30,14 @@ pub async fn create_vm(kvm_fd: RawFd, capacity: usize) {
     unsafe { create_vm_fd(kvm_fd, 0).expect("create_vm_fd_ERROR") };
     // assign memory to vm
     let ram_region = define_ram_region(capacity).unwrap();
+    let mem = KvmUserspaceMemoryRegion {
+        slot: 0 as __u32,
+        flags: 0 as __u32,
+        guest_phys_addr: ram_region as __u64,
+        memory_size: capacity as __u64,
+        userspace_addr: ram_region as __u64,
+    };
+
     ioctl_write_ptr_bad!(
         set_memory_region,
         request_code_write!(
@@ -36,7 +47,7 @@ pub async fn create_vm(kvm_fd: RawFd, capacity: usize) {
         ),
         KvmUserspaceMemoryRegion
     );
-    let ret = unsafe { set_memory_region(kvm_fd, std::ptr::addr_of!(ram_region)) };
+    let ret = unsafe { set_memory_region(kvm_fd, std::ptr::addr_of!(mem)) };
     if ret.is_err() {
         println!("{ram_region:?}");
     }
@@ -46,20 +57,24 @@ pub async fn create_vm(kvm_fd: RawFd, capacity: usize) {
     Set aside memory region for the virtual machine
     Result -> Done, invalid data input or error
 */
-fn define_ram_region(capacity: usize) -> Result<KvmUserspaceMemoryRegion, String> {
+fn define_ram_region(capacity: usize) -> Result<*mut c_void, String> {
     let prot_flags = ProtFlags::from_bits(PROT_READ | PROT_WRITE).unwrap();
     let map_flags = MapFlags::from_bits(MAP_PRIVATE | MAP_ANONYMOUS | MAP_NORESERVE).unwrap();
-    let mem = unsafe { mmap(null_mut(), capacity, prot_flags, map_flags, -1, 0) }.unwrap();
+    let mem = unsafe {
+        mmap(
+            None,
+            NonZeroUsize::new(capacity).unwrap(),
+            prot_flags,
+            map_flags,
+            -1,
+            0,
+        )
+    }
+    .unwrap();
     let _mlocked = unsafe {
         mlock(mem, capacity).unwrap();
     };
-    Ok(KvmUserspaceMemoryRegion {
-        slot: 0 as __u32,
-        flags: 0 as __u32,
-        guest_phys_addr: 0, // why does this become 0x10000 if you run an strace
-        memory_size: capacity as __u64, // this also just pops off
-        userspace_addr: mem as __u64, // this does not exist
-    })
+    Ok(mem)
 }
 
 pub fn get_kvm_fd() -> RawFd {
